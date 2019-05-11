ae4b2169f49af713a996dca2d3140b1b7536988c
Arnav Ashank <arnavashank.5@gmail.com>
2019-05-11 22:30:48 +0530

    Changed titanic_data_analysis.py

--- titanic_data_analysis.py
+++ titanic_data_analysis.py
@@ -21,3 +21,25 @@ import seaborn as sb
 gender=pd.read_csv('gender_submission.csv')
 test=pd.read_csv('test.csv')
 train=pd.read_csv('train.csv')
+
+# Assessing the data
+
+gender.info()
+
+train.info()
+
+train.describe(include='all')
+
+# Assessing the first few columns.
+
+train.head()
+
+# Dataset Issues
+
+# PassengerId column is of int type.
+
+# Cabin information missing for most of the passengers.
+
+# For the age column also dataset is missing which can prove to be a deciding factor in predicting survival.
+
+# Two passengers having no information of Embarked ports.
